798a819d5be3e395db7ed4375d52b05ad68d0ebd
Pyetro Ferreira <pyetroferreirasouzza9@gmail.com>
2025-12-29 18:27:33 -0300

    Iniciando tela gráfica

---
+++ script.js
@@ -0,0 +1,8 @@
+var numero = document.getElementsByClassName('7,8,9,4,5,6,1,2,3,0');
+function adicionarNumero(numero) {
+    const display = document.getElementById('display');
+    numero.value = '';
+    display.value += numero;
+        innerHTML.display('numero')
+    
+}
